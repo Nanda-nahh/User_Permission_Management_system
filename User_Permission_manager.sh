3fef0bc211b50623ca061a324d7479d05afdeba8
nandananandahh@gmail.com <nandananandahh@gmail.com>
2026-05-29 20:25:14 +0530

    removed all deleted files

---
+++ User_Permission_manager.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+clear
+
+echo "                                                         "
+echo "           USER & PERMISSION MANAGEMENT SYSTEM           "
+echo " "
+
+echo -n "Enter the username: "
+read username
+
+if  id "$username" &>/dev/null
+then
+    
+    echo "user already exist"
+else
+    useradd "$username"
+    echo "user created successfully"
+fi
